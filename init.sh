c34a34070baeb62b706dd99508f60b4a69ec6020
SauFung <saufungL@outlook.com>
2024-05-10 20:07:39 +0800

    change in 20240510

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+
+
+Init(){
+    local var=$1
+    if [[ $var = "nvim" ]]; then
+#        ln -s "$PWD"/nvim ~/.config/nvim
+        echo "Set Neovim Done"
+        echo "haha"
+    fi
+
+}
+
+#Init "$1"
+
+
--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -3,20 +3,6 @@ M.github_url = "https://ghproxy.org/https://github.com/"
 
 M.im_select = "C:/Env/im-select/im-select.exe"
 
-
--- 0 = Linux
--- 1 = Windows
--- 2 = 其他
-M.system = function ()
-  local system_id = vim.loop.os_uname().sysname
-  if system_id == "Linux" then
-    return 0
-  elseif system_id == "Windows_NT" then
-    return 1
-  else
-    return 2
-  end
-end
 -- M.github_url = "https://kkgithub.com/"
 
 -- 判断系统的方法
@@ -33,17 +19,33 @@ end
 -- vim.fn.has("win64")
 -- 若是返回 1，则为 Windows
 
-
-
-
-
 M.clangd_path = function ()
   if is_win then
-    return {"C:/Env/mingw64/bin/clangd.exe"}
+    return { "C:/Env/mingw64/bin/clangd.exe" }
   else
     return { "clangd" }
   end
 end
+
+
+-- 0 = Linux
+-- 1 = Windows
+-- 2 = 其他
+M.system = function ()
+  local system_id = vim.loop.os_uname().sysname
+  if system_id == "Linux" then
+    return 0
+  elseif system_id == "Windows_NT" then
+    return 1
+  else
+    return 2
+  end
+end
+
+
+
+
+
 
 return M
 
